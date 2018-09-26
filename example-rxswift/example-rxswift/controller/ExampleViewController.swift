52c9d106d96bd61e0cbc9d3a95d292a334b7ee3a
Victor Baleeiro <victor@translucentcomputing.com>
2018-09-26 17:14:17 -0300

    [config][dev] updated RxAlamofire pod and code test

--- example-rxswift/example-rxswift/controller/ExampleViewController.swift
+++ example-rxswift/example-rxswift/controller/ExampleViewController.swift
@@ -10,15 +10,16 @@ import Foundation
 import UIKit
 import RxSwift
 import RxCocoa
+import RxAlamofire
 
 class ExampleViewController: ViewController {
     
     //-------------------------------------------------------------------------------------------------------------
     // MARK: Properties
     //-------------------------------------------------------------------------------------------------------------
-    @IBOutlet weak var txtFirstName: UITextField!
-    @IBOutlet weak var txtLastName: UITextField!
-    @IBOutlet weak var lblCombine: UILabel!
+    // Views
+    @IBOutlet weak var tbvIssues: UITableView!
+    @IBOutlet weak var sbarIssues: UISearchBar!
     
     
     //-------------------------------------------------------------------------------------------------------------
@@ -36,18 +37,37 @@ class ExampleViewController: ViewController {
     // MARK: Setup
     //-------------------------------------------------------------------------------------------------------------
     func setupData() {
+        let url = URL.init(string: "https://jsonplaceholder.typicode.com/todos/1")
         
-        //Delegate
-        txtFirstName.delegate = self
-        txtLastName.delegate = self
+        // MARK: URLSession simple and fast
+//        let session = URLSession.shared
+//
+//        _ = session.rx
+//            .json(url: url!)
+//            .observeOn(MainScheduler.instance)
+//            .subscribe { print($0) }
+//
+//        // validation
+//        _ = request(.get, url!)
+//            .validate(statusCode: 200..<300)
+//            .validate(contentType: ["application/json"])
+//            .responseJSON()
+//            .observeOn(MainScheduler.instance)
+//            .subscribe { print($0) }
         
-        //Reative
-        Observable.combineLatest(txtFirstName.rx.text, txtLastName.rx.text) {
-            text1, text2 -> String in
-            return text1! + " " + text2!
-            }
-            .map { "Complete name: \(self.checkLabelCharLimits(str: $0.description))" }
-            .bind(to: lblCombine.rx.text)
+        RxAlamofire.requestJSON(.get, url!)
+            .debug()
+            .subscribe(onNext: { [weak weakSelf=self] (r, json) in
+                
+                // Tratando o sucesso
+                if let dict = json as? [String: AnyObject] {
+                    let titulo = dict["title"] as! String
+                    weakSelf?.exibeSucesso(titulo)
+                }
+                
+                }, onError: { [weak weakSelf=self] (error) in
+                    weakSelf?.exibeErro()
+            })
             .disposed(by: disposeBag)
     }
     
@@ -63,15 +83,14 @@ class ExampleViewController: ViewController {
     //-------------------------------------------------------------------------------------------------------------
     // MARK: Aux
     //-------------------------------------------------------------------------------------------------------------
-    func checkLabelCharLimits(str: String) -> String {
-        return (str.count > 10) ? (str[..<str.index(str.startIndex, offsetBy: 9)] + "...") : str
+    private func exibeErro() {
+        print("Ocorreu um erro ao tentar realizar a requisição...")
+    }
+    
+    private func exibeSucesso(_ titulo: String) {
+        print("Sucesso ao fazer a requisição. Titulo: \(titulo)")
     }
 }
 
-extension ExampleViewController: UITextFieldDelegate {
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-}
+
+
